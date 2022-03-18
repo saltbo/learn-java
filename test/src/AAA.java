581f654ae9fe48572a90a8cb6b8ff0674ad3bd97
saltbo <saltbo@foxmail.com>
2022-03-18 18:02:15 +0800

    feat: restudy the class knowledge

---
+++ test/src/AAA.java
@@ -0,0 +1,12 @@
+public class AAA {
+
+    public int c;
+
+    public int su() {
+        return this.c + this.a();
+    }
+
+    private int a() {
+        return 1;
+    }
+}
--- test/src/test.java
+++ test/src/test.java
@@ -1,5 +1,24 @@
+import static java.lang.System.out;
+
 public class test {
+
     public static void main(String[] args) {
-        System.out.printf("112233");
+        String abc = "123";
+        out.printf("112233: %s\n", abc);
+        abc = "111";
+        if (!abc.equals("123")) {
+            out.println(abc);
+        }
+
+        out.println(sum(1,2));
+
+        AAA aaa = new AAA();
+        aaa.c = 10;
+        out.println(aaa.su());
     }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
 }
